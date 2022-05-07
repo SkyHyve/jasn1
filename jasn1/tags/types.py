aad80eee1361abeb885751a1f1f8702babf4a6ef
Matthew Graham <matthew.grahan@skyhyve.com.au>
2022-05-07 15:13:06 +1000

    initial commit

---
+++ jasn1/tags/types.py
@@ -0,0 +1,38 @@
+OCTET_STRING = "OCTET STRING"
+BOOLEAN = "BOOLEAN"
+INTEGER = "INTEGER"
+BIT_STRING = "BIT_STRING"
+OCTET_STRING = "OCTET_STRING"
+NULL = "NULL"
+OBJECT_IDENTIFIER = "OBJECT_IDENTIFIER"
+ObjectDescriptor = "ObjectDescriptor"
+ENUMERATED = "ENUMERATED"
+EMBEDDED_PDV = "EMBEDDED_PDV"
+UTF8_STRING = "UTF8_STRING"
+SEQUENCE = "SEQUENCE"
+SET = "SET"
+PrintableString = "PrintableString"
+T61String = "T61String"
+IA5String = "IA5String"
+UTCTime = "UTCTime"
+
+TYPE_MAP = {
+    0: OCTET_STRING,
+    1: BOOLEAN,
+    2: INTEGER,
+    3: BIT STRING,
+    4: OCTET STRING,
+    5: NULL,
+    6: OBJECT IDENTIFIER,
+    7: ObjectDescriptor,
+    10: ENUMERATED,
+    11: EMBEDDED PDV,
+    12: UTF8 STRING,
+    16: SEQUENCE,
+    17: SET,
+    19: PrintableString,
+    20: T61String,
+    22: IA5String,
+    23: UTCTime,
+}
+INVERSE_TYPE_MAP = {TYPE_MAP[x]: x for x in TYPE_MAP}
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="jasn1",
+    version="0.0.1",
+    author="Matthew Laurence Willaim Graham",
+    description="Parsing ASN.1 data to JSON and vice-versa",
+    packages=["jasn1"]
+)
